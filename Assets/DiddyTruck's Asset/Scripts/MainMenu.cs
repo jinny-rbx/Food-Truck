88ceebc9afd4920b96d82e65a6111213e8f30c29
jinny-rbx <jinnysstuff@gmail.com>
2026-08-20 20:18:11 +0800

    temp fix

--- Assets/DiddyTruck's Asset/Scripts/MainMenu.cs
+++ Assets/DiddyTruck's Asset/Scripts/MainMenu.cs
@@ -1,15 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        LevelManager.Instance.gameObject.SetActive(true);
-        LevelManager.Instance.LoadScene("Game", "CrossFade"); 
+        // Try finding Instance if null
+        if (LevelManager.Instance == null)
+        {
+            LevelManager.Instance = FindFirstObjectByType<LevelManager>();
+        }
+
+        if (LevelManager.Instance != null)
+        {
+            LevelManager.Instance.gameObject.SetActive(true);
+            LevelManager.Instance.LoadScene("Game", "CrossFade");
+        }
+        else
+        {
+            Debug.LogError("MainMenu: No LevelManager found in the scene!");
+        }
     }
 
     public void Quit()
