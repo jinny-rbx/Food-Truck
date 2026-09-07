77656fa9f78c08c6d10b1094c1f14b9c7eb9e1cc
jinny-rbx <jinnysstuff@gmail.com>
2026-09-07 09:14:38 +0800

    dialogue

--- Assets/DiddyTruck's Asset/Scripts/Dialogue.cs
+++ Assets/DiddyTruck's Asset/Scripts/Dialogue.cs
@@ -7,7 +7,11 @@ public class StartDialogue : MonoBehaviour
 {
     private void Start()
     {
-        DialogueController.instance.NewDialogueInstance("Hey! Hope you are doing well. This is a test dialogue instance for the new dialogue package EvMeshPro", "character_diddy");
-        DialogueController.instance.NewDialogueInstance("This is an [NAMES]easy to use package[/NAMES] to give developers a good looking and simple dialogue system.");
+        DialogueController.instance.NewDialogueInstance("Hey! Hope you are doing well.. Welcome to the tutorial!", "character_diddy");
+        DialogueController.instance.NewDialogueInstance("Use [NAMES]WASD[/NAMES] to move your character! Give it a try!");
+        DialogueController.instance.NewDialogueInstance("The GREEN and ORANGE bar on top left of your screen are your [NAMES]Health[/NAMES] and [NAMES]Fuel[/NAMES].");
+        DialogueController.instance.NewDialogueInstance("While the bar at the top middle of your screen is the [NAMES]Satisfaction[/NAMES] meter");
+        DialogueController.instance.NewDialogueInstance("You can collide with these [NAMES]PURPLE[/NAMES] cubes to complete your objective! and GREEN cubes to refill your fuel");
+        DialogueController.instance.NewDialogueInstance("While the RED eats your fuel and the BLACK one destroys you..");
     }
 }
